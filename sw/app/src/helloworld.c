9b5965efa8efa141e8a46e1c1512830501ca31a2
ntargon <argon19951128@gmail.com>
2025-06-14 20:40:55 +0900

    feat: implement UART echo functionality in helloworld application

--- sw/app/src/helloworld.c
+++ sw/app/src/helloworld.c
@@ -17,17 +17,46 @@
  *   ps7_uart    115200 (configured by bootrom/bsp)
  */
 
+#include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 #include "platform.h"
 #include "xil_printf.h"
-
 
 int main()
 {
     init_platform();
 
-    print("Hello World\n\r");
-    print("Successfully ran Hello World application");
+    print("UART Echo Application\n\r");
+    print("Type characters to see them echoed back.\n\r");
+    print("Use <CR> as delimiter.\n\r");
+
+    char buf[256] = {0}; // Buffer to hold input characters
+    uint32_t index = 0;
+
+    while (1) {
+        // Read character from UART
+        char c = inbyte();
+
+        if (index > sizeof(buf) - 1) {
+            // skip if buffer is full
+            c = '\r'; // Use carriage return to indicate buffer overflow
+        }
+        buf[index++] = c;
+
+        // If character is CR, add newline
+        if (c == '\r') {
+            buf[index++] = '\n';
+            buf[index++] = '\0'; // Null-terminate the string
+            // Echo the input back
+            print(buf); // Print the buffer content
+
+            index = 0; // Reset index for next line
+            memset(buf, 0, sizeof(buf)); // Clear buffer
+        }
+    }
+
+    // Note: This will never be reached due to infinite loop
     cleanup_platform();
     return 0;
 }
